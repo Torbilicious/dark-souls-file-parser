3b37ccbf455b2d43a5f29d48327f1f064c7540b5
Torben Binder <t.binder@steinberg.de>
2019-01-19 15:26:31 +0100

    Restructure project a bit

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,8 @@
 
 
 int main() {
-//    const char *fileName = "/Volumes/Secured/Projects/cpp/dark-souls-file-parser/DRAKS0005.sl2";
-    const char *fileName = "/Volumes/Secured/Projects/cpp/dark-souls-file-parser/DRAKS0005.sl2.2";
+//    const char *fileName = "/Volumes/Secured/Projects/cpp/dark-souls-file-parser/resources/DRAKS0005.sl2";
+    const char *fileName = "/Volumes/Secured/Projects/cpp/dark-souls-file-parser/resources/DRAKS0005.sl2.2";
 
     auto reader = new Reader(fileName);
 
